94443b1d4db0967c94e830860087466a6a13fe14
awesomerapha <raphael.morgenshtern@gmail.com>
2019-10-08 11:26:09 -0400

    d

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-function videoGame(title, genre, yearOfRelease) {
-    this.title = title;
-    this.genre = genre;
-    this.yearOfRelease = yearOfRelease;
-    this.age = function() {
-        return new Date().getUTCFullYear() - this.yearOfRelease;
+//one class to create the object person, album, movie ****Model
+//take arguments
+class gameCreator{
+    construction(yearOfRelease, title, genre){
+        this.yearOfRelease = yearOfRelease;
+        this.title = title;
+        this.genre = genre;
     }
+    calculateAge(){
+        return new Date().getUTCFullYear - this.yearOfRelease;
+    }
+}
+
+
+// *****View // class for the UI which contains methods
+
+//creates/inserts the html for the UI DISPLAY
+
+//clear fields method
+
+//remove something/object
+
+
+
+
+//Controller combines UI and model
+//event handler with function
+function eventListeners() {
+    form.addEventListener('submit', ); //Figure out
+    display.addEventListener('click', ); //Figure Out
+}
+eventListeners();
+
+//get values
+
+
+//instantiate new class/object album with variable name
+function createGame() {
     
 }
+//instantiate new UI const ui = new UI()
+
+//call relevant methods
+
+
+//seperate event handler for removing/deleting
